82084290b125068e5452a050789f3046568bac2b
adigapranava <66902872+adigapranava@users.noreply.github.com>
2020-06-20 11:04:10 +0530

    Delete diff_urllib_requests.py

--- diff_urllib_requests.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import requests
-
-url1=urllib.request.urlopen("https://ipinfo.io/")
-url2=requests.get("https://ipinfo.io/")
-
-print(url1.read().decode())
-
-print(url2.text)
